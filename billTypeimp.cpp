66b5f0dbe27518ab85faa6a6ae7e67162fa39867
Brian <nieh_brian@yahoo.com>
2022-09-28 23:30:26 -0700

    Added functions

---
+++ billTypeimp.cpp
@@ -0,0 +1,55 @@
+/*
+Name: Brian Nieh, NSHE: 5008139573, CLASS-SECTION: 1005, ASSIGNMENT:3
+Description: To set the patients and doctors data; print the bill of a
+patient
+Input: Develop personType class that can be useful to set first and last
+names from personType class; set the patients data using setters or
+constructors from patienType class; set the doctors data using setters
+or constructors from doctorType class; set the birth date(mm, dd, year)
+of a patient from dateType class; set the charges(pharmacy, room rent,
+doctor fee) of a patient from billType class
+Output: Write output to console; print error messages for Invalid IDs,
+missing specialty, invalid biryh month and year;
+check the sample output for reference
+*/
+#include <iostream>
+#include "billType.h"
+// Default Constructor
+// The member variables ID, pharmacyCharges, roomRent, doctorFee,
+// have to be initialized; no need to initialize pType here
+billType::billType()
+{
+}
+// Parameterized Constructor
+// The member variables ID, pharmacyCharges, roomRent, doctorFee,
+// are set according to the parameters.
+// Also set pType with patientObj
+billType::billType(string id, double phCharges, double rRent,
+                   double docFee, patientType patientObj)
+{
+}
+// Function to setInfo of a bill
+// The member variables ID, pharmacyCharges, roomRent, doctorFee,
+// are set according to the parameters.
+void billType::setInfo(string id, double phCharges, double rRent,
+                       double docFee)
+{
+}
+// Function to setID according to the parameter
+void billType::setID(string id)
+{
+}
+// Function to return pharmacyCharges + roomRent + doctorFee
+double billType::billingAmount()
+{
+}
+// Function to return the ID.
+string billType::getID()
+{
+}
+// Function to output the first name and last name using pType obj
+// Function to output Billing details like ID, pharmacyCharges,
+// roomRent, doctorFee and finally total charges
+void billType::printBill()
+{
+}
