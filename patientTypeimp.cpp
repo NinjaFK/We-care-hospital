c0f0bd2d2298638a64c78eaa2806d8313ee5ad25
Brian <nieh_brian@yahoo.com>
2022-09-29 11:45:38 -0700

    added functionality

--- patientTypeimp.cpp
+++ patientTypeimp.cpp
@@ -14,7 +14,6 @@ check the sample output for reference
 */
 #include <iostream>
 #include "patientType.h"
-#include "dateType.h"
 
 // Parameterized Constructor
 // The patient details like ID, first name, last name,
@@ -27,6 +26,7 @@ patientType::patientType(string id = "", string fName = "", string lName = "",
     ID = id;
     setName(fName, lName);
     dateOfBirth = dateType(bMth, bDay, bYear);
+    attendingPhysicain = doctorType(docFrName, docLaName, docSpl);
 }
 // Function to setInfo of a patient
 // The patient details like ID, first name, last name,
@@ -37,41 +37,59 @@ void patientType::setInfo(string id = "", string fName = "", string lName = "",
                           string docFrName = "", string docLaName = "",
                           string docSpl = "")
 {
+    ID = id;
+    setName(fName, lName);
+    dateOfBirth.setDate(bMth, bDay, bYear);
+    attendingPhysicain.setName(docFrName, docLaName);
+    attendingPhysicain.setSpecialty(docSpl);
 }
 // Function to set the ID according to the parameter.
 void patientType::setID(string id)
 {
+    ID = id;
 }
 // Function to set the birthDate.
 // bDay, bMth, bYear are set according to the parameters.
 // if none given they are set to the above default values
 void patientType::setBirthDate(int bDay = 1, int bMth = 1, int bYear = 1910)
 {
+    dateOfBirth.setDate(bMth, bDay, bYear);
 }
 // Function to set the doctor's first name and last name
 // according to the parameters.
 void patientType::setDoctorName(string fName = "", string lName = "")
 {
+    attendingPhysicain.setName(fName, lName);
 }
 // Function to set the doctor's specialty according to the parameter
 void patientType::setDoctorSpl(string spl = "")
 {
+    attendingPhysicain.setSpecialty(spl);
 }
 // Function to return the ID
 string patientType::getID() const
 {
+    return ID;
 }
 // Function to return the year
 int patientType::getBirthYear() const
 {
+    return dateOfBirth.getYear();
 }
 // Function to return doctor's first name.
 string patientType::getDoctorFName() const
 {
+    return attendingPhysicain.getFirstName();
 }
 // Function to output the first name and last name of patient
 // Function to output doctors first name, last name, and specialty
 // using doctorType object(attendingPhysicain)
 void patientType::print() const
 {
+    cout << getFirstName() << " "
+         << getLastName << " "
+         << attendingPhysicain.getFirstName() << " "
+         << attendingPhysicain.getLastName() << " "
+         << attendingPhysicain.getSpecialty() << " "
+         << '\n';
 }
